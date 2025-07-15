7e83865ac886c09a9023dfe41ea35e8a884b6aba
Noordeep Singh <104714650+Noordeepsinghv@users.noreply.github.com>
2025-07-15 21:39:35 +0530

    Add files via upload

---
+++ Simple Interest Calculator/script.js
@@ -0,0 +1,29 @@
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal <= 0) {
+        alert("Please enter a positive number for Principal");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var interest = (principal * rate * years) / 100;
+    var totalAmount = parseFloat(principal) + parseFloat(interest);
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML =
+        "If you deposit <span id='output'>" + principal + "</span>,<br>" +
+        "at an interest rate of <span id='output'>" + rate + "%</span>.<br>" +
+        "You will receive an amount of <span id='output'>" + totalAmount.toFixed(2) + "</span>,<br>" +
+        "in the year <span id='output'>" + year + "</span>.";                   
+}
+function updateRate() {
+    var rateValue = document.getElementById("rate").value;
+    document.getElementById("rateValue").value = rateValue;
+}
+function updateRateValue() {
+    var rate = document.getElementById("rateValue").value;
+    document.getElementById("rate").value = rate;
+}
